289f25da9a2cb8be80d0507a39e095f130e70b38
fjhjc01 <fjhjc01@bu.edu>
2015-10-30 22:14:11 -0400

    almost done the drawing chart part

--- MET673/public/chart.js
+++ MET673/public/chart.js
@@ -1,88 +1,28 @@
 google.load('visualization', '1', {'packages':['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
-// google.setOnLoadCallback(drawChart);
+google.setOnLoadCallback(drawChart);
 
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
-// function drawSleepChart(arr, num) {
-//   var data = new google.visualization.DataTable();
-//   data.addColumn('string', 'Days');
-//   data.addColumn('number', 'Hours');
-//   if (num == 0) {
-//     for (var i = 0; i < arr.length; i++) {
-//       data.addRows([
-//         [arr[i].timestamp, arr[i].sleep_hours],
-//       ]);
-//     }
-//   }
-//   // Set chart options
-//   var options = {'width':800,
-//                  'height':600};
-//
-//   // Instantiate and draw our chart, passing in some options.
-//   var chart = new google.visualization.BarChart(document.getElementById('sleep_hours'));
-//   chart.draw(data, options);
-// }
-function drawChart() {
+function drawSleepChart(arr, num) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Days');
+  data.addColumn('number', 'Hours');
+  if (num == 0) {
+    for (var i = 0; i < arr.length; i++) {
+      data.addRows([
+        [arr[i].timestamp, arr[i].sleep_hours],
+      ]);
+    }
+  }
+  // Set chart options
+  var options = {'width':800,
+                 'height':600};
 
-// Create the data table.
-// var data = new google.visualization.DataTable();
-// data.addColumn('string', 'Days');
-// data.addColumn('number', 'Hours');
-// data.addRows([
-//   ['Monday', 9.2],
-//   ['Tuesday', 8.5],
-//   ['Wednesday', 8.8],
-//   ['Thursday', 9],
-//   ['Friday', 9.1]
-// ]);
-//
-// // Set chart options
-// var options = {'width':800,
-//                'height':600};
-//
-// // Instantiate and draw our chart, passing in some options.
-// var chart = new google.visualization.BarChart(document.getElementById('sleep_hours'));
-// chart.draw(data, options);
-
-var data1 = new google.visualization.DataTable();
-data1.addColumn('string', 'Days');
-data1.addColumn('number', 'Steps');
-data1.addRows([
-  ['Monday', 300],
-  ['Tuesday', 200],
-  ['Wednesday', 300],
-  ['Thursday', 400],
-  ['Friday', 300]
-]);
-
-// Set chart options
-var options = {'width':800,
-               'height':600};
-
-// Instantiate and draw our chart, passing in some options.
-var chart = new google.visualization.ColumnChart(document.getElementById('running_steps'));
-chart.draw(data1, options);
-
-var data2 = new google.visualization.DataTable();
-data2.addColumn('string', 'Days');
-data2.addColumn('number', 'Calorie');
-data2.addRows([
-  ['Monday', 3000],
-  ['Tuesday', 2000],
-  ['Wednesday', 3000],
-  ['Thursday', 4000],
-  ['Friday', 3000]
-]);
-
-// Set chart options
-var options = {'width':800,
-               'height':600};
-
-// Instantiate and draw our chart, passing in some options.
-var chart = new google.visualization.LineChart(document.getElementById('calorie'));
-chart.draw(data2, options);
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.BarChart(document.getElementById('sleep_hours'));
+  chart.draw(data, options);
 }
--- MET673/public/personal.js
+++ MET673/public/personal.js
@@ -12,10 +12,20 @@ $(document).ready(function() {
   });
 });
 
+socket.on('chart data', function(arr) {
+  console.log("here");
+  drawChart(arr);
+});
+
 google.load('visualization', '1', {'packages':['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
 // google.setOnLoadCallback(drawSleepChart);
+function drawChart(arr) {
+  drawSleepChart(arr);
+  drawStepsChart(arr);
+  drawCalorieChart(arr);
+}
 
 function drawSleepChart(arr) {
   var data = new google.visualization.DataTable();
@@ -30,13 +40,45 @@ function drawSleepChart(arr) {
   // Set chart options
   var options = {'width':800,
                  'height':600};
-
   // Instantiate and draw our chart, passing in some options.
   var chart = new google.visualization.BarChart(document.getElementById('sleep_hours'));
   chart.draw(data, options);
 }
 
-socket.on('chart data', function(arr) {
-  console.log("here");
-  drawSleepChart(arr);
-});
+function drawStepsChart(arr) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Days');
+  data.addColumn('number', 'Steps');
+  for (var i = 0; i < arr.length; i++) {
+    var date = new Date(arr[i].timestamp);
+    data.addRows([
+      [lookUpTable[date.getDay() - 1], arr[i].steps]
+    ]);
+  }
+  // Set chart options
+  var options = {'width':800,
+                 'height':600};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.ColumnChart(document.getElementById('running_steps'));
+  chart.draw(data, options);
+}
+
+function drawCalorieChart(arr) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Days');
+  data.addColumn('number', 'Calorie');
+  for (var i = 0; i < arr.length; i++) {
+    var date = new Date(arr[i].timestamp);
+    data.addRows([
+      [lookUpTable[date.getDay() - 1], arr[i].calorie]
+    ]);
+  }
+  // Set chart options
+  var options = {'width':800,
+                 'height':600};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.LineChart(document.getElementById('calorie'));
+  chart.draw(data, options);
+}
